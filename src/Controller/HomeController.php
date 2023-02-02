858c5962f4f4e40cdf5d9bffdba42d3b3a1f85ae
Pawel <studentpawel2000@gmail.com>
2023-02-02 11:51:04 +0100

    Controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class HomeController {
+    
+    
+    /**
+     * @Route ("/", name="home")
+     */
+    
+    public function index(){
+        die("Home");
+    }
+}
